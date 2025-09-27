cb4dc8f82ed3baa0f396fb7e335a9d9a1bce280a
Rishabh503 <rishabhtripathi2022@gmail.com>
2025-09-27 14:07:01 +0530

    three conrolle

--- server/controllers/aiController.js
+++ server/controllers/aiController.js
@@ -1,7 +1,8 @@
 import { clerkClient } from "@clerk/express";
 import OpenAI from "openai";
 import { sql } from "../configs/db.js";
-
+import axios from "axios";
+import {v2 as cloudinary} from 'cloudinary'
 const AI = new OpenAI({
   apiKey: process.env.GEMINI_API_KEY,
   baseURL: "https://generativelanguage.googleapis.com/v1beta/openai/",
@@ -22,31 +23,115 @@ export const generateArticle = async (req, res) => {
     const response = await AI.chat.completions.create({
       model: "gemini-2.0-flash",
       messages: [
-       {
+        {
           role: "user",
           content: prompt,
         },
       ],
-      temperature:0.7,
-      max_tokens:length
+      temperature: 0.7,
+      max_tokens: length,
     });
 
-    const content=response.choices[0].message.content
-    console.log(content)
+    const content = response.choices[0].message.content;
+    console.log(content);
     await sql`INSERT INTO creations(user_id,prompt,content,type)
     values (${userId},${prompt},${content},'article')`;
-    if(plan!=='premium'){ 
-        await clerkClient.users.updateUserMetadata(userId,{
-            privateMetadata:{
-                free_usage:free_usage+1
-            }
-        })
+    if (plan !== "premium") {
+      await clerkClient.users.updateUserMetadata(userId, {
+        privateMetadata: {
+          free_usage: free_usage + 1,
+        },
+      });
     }
 
-    res.json({success:true,content})
+    res.json({ success: true, content });
   } catch (error) {
+    console.log(error.message);
+    res.json({ success: false, message: error.message });
+  }
+};
+export const generateBlogTitle = async (req, res) => {
+  try {
+    const { userId } = req.auth();
+    const { prompt } = req.body;
+    const plan = req.plan;
+    const free_usage = req.free_usage;
 
-    console.log(error.message)
-     res.json({success:false,message:error.message})
+    if (plan !== "premium" && free_usage >= 10) {
+      return res.json({
+        success: false,
+        message: "limit has reached , upgrade  for more",
+      });
+    }
+    const response = await AI.chat.completions.create({
+      model: "gemini-2.0-flash",
+      messages: [
+        {
+          role: "user",
+          content: prompt,
+        },
+      ],
+      temperature: 0.7,
+      max_tokens: 100,
+    });
+
+    const content = response.choices[0].message.content;
+    console.log(content);
+    await sql`INSERT INTO creations(user_id,prompt,content,type)
+    values (${userId},${prompt},${content},'blog-title')`;
+    if (plan !== "premium") {
+      await clerkClient.users.updateUserMetadata(userId, {
+        privateMetadata: {
+          free_usage: free_usage + 1,
+        },
+      });
+    }
+
+    res.json({ success: true, content });
+  } catch (error) {
+    console.log(error.message);
+    res.json({ success: false, message: error.message });
+  }
+};
+export const generateImage = async (req, res) => {
+  try {
+    const { userId } = req.auth();
+    const { prompt, publish } = req.body;
+    const plan = req.plan;
+
+    if (plan !== "premium") {
+      return res.json({
+        success: false,
+        message: "Subscribe to use this feature",
+      });
+    }
+
+    const formData = new FormData();
+    formData.append("prompt", prompt);
+    const {data}=await axios.post('https://clipdrop-api.co/text-to-image/v1',formData,{
+        headers:{'x-api-key':process.env.CLIPDROP_API_KEY},
+        responseType:"arraybuffer"
+    })
+    console.log(data)
+    const base64Image=`data:image/png;base64,${Buffer.from(data,'binary').toString('base64')}`
+
+    const {secure_url}=await cloudinary.uploader.upload(base64Image)
+    console.log(secure_url)
+
+
+    await sql`INSERT INTO creations(user_id,prompt,content,type,publish)
+    values (${userId},${prompt},${secure_url},'image',${publish?? false})`;
+    if (plan !== "premium") {
+      await clerkClient.users.updateUserMetadata(userId, {
+        privateMetadata: {
+          free_usage: free_usage + 1,
+        },
+      });
+    }
+
+    res.json({ success: true, content:secure_url });
+  } catch (error) {
+    console.log(error.message);
+    res.json({ success: false, message: error.message });
   }
 };
