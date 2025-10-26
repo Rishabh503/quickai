08dbef9ee483c1d67b2d1161cfe1b9ec1956d3b8
Rishabh503 <rishabhtripathi2022@gmail.com>
2025-10-26 17:09:57 +0530

    added the new logo :

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -6,6 +6,7 @@ import { ArrowRight } from "lucide-react";
 import { assets } from "../assets/assets";
 import { useClerk, UserButton, useUser } from "@clerk/clerk-react";
 // import logi.png from "../assets"
+// import logo from "../../public"
 const Navbar = () => {
   const navigate = useNavigate();
 
@@ -16,9 +17,9 @@ const Navbar = () => {
   return (
     <div className="fixed z-5 w-full bg-[#171212] text-white backdrop-blur-2xl flex justify-between items-center py-3 px- sm:px-10 xl:px-5">
       <img
-        src={assets.logo}
+        src="../../logi.png"
         alt="logo"
-        className="w-32 sm:w-44 cursor-pointer"
+        className="w-32 h-16 sm:w-44 cursor-pointer"
         onClick={() => navigate("/")}
       />
 
